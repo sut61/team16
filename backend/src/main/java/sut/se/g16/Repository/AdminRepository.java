69c3e1195ed4c3da72f80a30ab4876d6a13013fb
pupchopper <pup.puppup94@gmail.com>
2019-01-25 15:00:35 +0700

    add new repository - close #67

---
+++ backend/src/main/java/sut/se/g16/Repository/AdminRepository.java
@@ -0,0 +1,14 @@
+package sut.se.g16.Repository;
+import sut.se.g16.Entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestController
+@EnableJpaRepositories
+@CrossOrigin(origins = "http://localhost:4200")
+@RepositoryRestResource
+public interface AdminRepository extends JpaRepository<AdminEntity, Long>{
+}
