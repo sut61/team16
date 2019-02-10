9eb55870137152c9c4c759c684708bae1cc87b28
sumitra40 <cc40.sumitra@gmail.com>
2019-02-10 20:02:38 +0700

    add Test Caes ReservationRoom and ReservationMeetingRoom -close#136

---
+++ backend/src/test/java/sut/se/g16/ReservationMeetingEventRoom.java
@@ -0,0 +1,388 @@
+package sut.se.g16;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import sut.se.g16.Entity.*;
+import sut.se.g16.Repository.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.net.SocketPermission;
+import java.util.Collections;
+import java.util.Date;
+import java.util.OptionalInt;
+import java.util.Set;
+
+import javax.sound.sampled.SourceDataLine;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ReservationMeetingEventRoom {
+
+	@Autowired
+	private ReservationRoomRepository reservationRoomRepository;
+	@Autowired
+	private ReservationMeetingEventRoomRepository reservationMeetingEventRoomRepository;
+	@Autowired
+	private HotelRepository hotelRepository;
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private PromotionRepository promotionRepository;
+	@Autowired
+	private RoomTypeRepository roomTypeRepository;
+
+	@Autowired
+	private MeetingEventRoomRepository meetingEventRoomRepository;
+	@Autowired
+	private MeetingEventRoomTypeRepository meetingEventRoomTypeRepository;
+	@Autowired
+	private RoomStatusRepository roomStatusRepository;
+	@Autowired
+	private TypeTimeRepository typeTimeRepository;
+	@Autowired
+	private FunctionRepository functionRepository;
+
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	private Validator validator;
+
+	@Before
+	public void setup() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+// Test Case Sprint #2
+	@Test
+	public void testVadidValueAllSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setDetail("Add Pillow2");
+		resermet.setReservationMeetingEventRoomDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		resermet.setReservationMeetingEventRoomDateOut(new Date(2100, 2 - 1, 1));
+		reservationMeetingEventRoomRepository.save(resermet);
+
+	}
+
+	@Test
+	public void testDetailCannotBeNullSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setDetail(null);
+		resermet.setReservationMeetingEventRoomDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		resermet.setReservationMeetingEventRoomDateOut(new Date(2018 - 1900, 2 - 1, 1));
+
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("1++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("1++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testDateInCannotBeNullSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setDetail("Add Pillow2");
+		resermet.setReservationMeetingEventRoomDateIn(null);
+		resermet.setReservationMeetingEventRoomDateOut(new Date(2018 - 1900, 1, 1));
+
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+		//	fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("2++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("2++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testDateOutCannotBeNullSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setDetail("Add Pillow2");
+		resermet.setReservationMeetingEventRoomDateIn(new Date(2018 - 1900, 1, 1));
+		resermet.setReservationMeetingEventRoomDateOut(null);
+
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("3++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("3++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testDetailLengthMax6SP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setDetail("Add Pillow2BlanketTowelBlanketTowelBlanketTowel");
+		resermet.setReservationMeetingEventRoomDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		resermet.setReservationMeetingEventRoomDateOut(new Date(2018 - 1900, 2 - 1, 1));
+
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("4++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("4++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testDetailLengthMin6SP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setDetail("Add dd3f");
+		resermet.setReservationMeetingEventRoomDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		resermet.setReservationMeetingEventRoomDateOut(new Date(2018 - 1900, 2 - 1, 1));
+
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("5++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("5++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testDetailStartWithAToZSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setDetail("#dd Pillow2");
+		resermet.setReservationMeetingEventRoomDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		resermet.setReservationMeetingEventRoomDateOut(new Date(2018 - 1900, 2 - 1, 1));
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("6++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("6++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testDetailMustBeUnique() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setDetail("Add Pillow2");
+		resermet.setReservationMeetingEventRoomDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		resermet.setReservationMeetingEventRoomDateOut(new Date(2018 - 1900, 2 - 1, 1));
+		entityManager.persist(resermet);
+		entityManager.flush();
+		ReservationMeetingEventRoomEntity resermet2 = new ReservationMeetingEventRoomEntity();
+		resermet2.setDetail("Add Pillow2");
+		resermet2.setReservationMeetingEventRoomDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		resermet2.setReservationMeetingEventRoomDateOut(new Date(2018 - 1900, 2 - 1, 1));
+
+		try {
+			entityManager.persist(resermet2);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.persistence.PersistenceException e) {
+			System.out.println("12====================");
+			e.printStackTrace();
+			System.out.println("====================");
+
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.println("122====================");
+			System.out.println(e);
+			e.printStackTrace();
+			System.out.println("====================");
+
+		}
+	}
+
+	@Test
+	public void testVadidValueMeetingEventRoomTypeAllSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewMeetingEventRoomTypeEntity(meetingEventRoomTypeRepository.findMeetingEventRoomTypeByName("??????"));
+		reservationMeetingEventRoomRepository.save(resermet);
+
+	}
+
+	@Test
+	public void testMeetingEventRoomTypeCannotBeNull() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewMeetingEventRoomTypeEntity(meetingEventRoomTypeRepository.findMeetingEventRoomTypeByName(null));
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testVadidValueMeetingEventRoomAllSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewMeetingEventRoomTypeEntity(meetingEventRoomTypeRepository.findMeetingEventRoomTypeByName("AA0000"));
+		reservationMeetingEventRoomRepository.save(resermet);
+
+	}
+
+	@Test
+	public void testMeetingEventRoomCannotBeNull() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewMeetingEventRoomTypeEntity(meetingEventRoomTypeRepository.findMeetingEventRoomTypeByName(null));
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+
+
+	@Test
+	public void testVadidValueFunctionAllSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewFunctionEntity(functionRepository.findByName("???????"));
+		reservationMeetingEventRoomRepository.save(resermet);
+
+	}
+
+	@Test
+	public void testFunctionCannotBeNull() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewFunctionEntity(functionRepository.findByName(null));
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testVadidValueRoomStatusAllSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewRoomStatusEntity(roomStatusRepository.findByName("???????"));
+		reservationMeetingEventRoomRepository.save(resermet);
+
+	}
+
+	@Test
+	public void testRoomStatusCannotBeNull() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewRoomStatusEntity(roomStatusRepository.findByName(null));
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testVadidValueTypeTimsAllSP2() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewTypeTimeEntity(typeTimeRepository.findBytypeTimeName("Morning"));
+		reservationMeetingEventRoomRepository.save(resermet);
+
+	}
+
+	@Test
+	public void testTypeTimeCannotBeNull() {
+		ReservationMeetingEventRoomEntity resermet = new ReservationMeetingEventRoomEntity();
+		resermet.setNewTypeTimeEntity(typeTimeRepository.findBytypeTimeName(null));
+		try {
+			entityManager.persist(resermet);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+}
---
+++ backend/src/test/java/sut/se/g16/ReservationRoom.java
@@ -0,0 +1,350 @@
+﻿package sut.se.g16;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import sut.se.g16.Entity.*;
+import sut.se.g16.Repository.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.net.SocketPermission;
+import java.util.Collections;
+import java.util.Date;
+import java.util.OptionalInt;
+import java.util.Set;
+
+import javax.sound.sampled.SourceDataLine;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ReservationRoom {
+
+	@Autowired
+	private ReservationRoomRepository reservationRoomRepository;
+	@Autowired
+	private ReservationMeetingEventRoomRepository reservationMeetingEventRoomRepository;
+	@Autowired
+	private HotelRepository hotelRepository;
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private PromotionRepository promotionRepository;
+	@Autowired
+	private RoomTypeRepository roomTypeRepository;
+
+	@Autowired
+	private MeetingEventRoomRepository meetingEventRoomRepository;
+	@Autowired
+	private MeetingEventRoomTypeRepository meetingEventRoomTypeRepository;
+	@Autowired
+	private RoomStatusRepository roomStatusRepository;
+	@Autowired
+	private TypeTimeRepository typeTimeRepository;
+	@Autowired
+	private FunctionRepository functionRepository;
+
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	private Validator validator;
+
+	@Before
+	public void setup() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	public void testVadidValueAll() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setCommentReserroom("Add Pees2");
+		reservationRoomRepository.save(reserroom);
+	}
+
+	@Test
+	public void testReserservationRoomCannotBeNull() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setCommentReserroom(null);
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			// assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testReservationRoomLength10T40() {
+		;
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setCommentReserroom("Add asadafgdhf1dfgfgfsdfddf");
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			// assertEquals(violations.size(), 1);
+		}
+	}
+
+	@Test
+	public void testDateIn1CannotBeNullSP2() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setCommentReserroom("Add Pillow2");
+		reserroom.setDateIn(null);
+		reserroom.setDateOut(new Date(2018 - 1900, 1, 1));
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("2++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("2++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testDateOut1CannotBeNullSP2() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setCommentReserroom("Add Pillow2");
+		reserroom.setDateIn(new Date(2018 - 1900, 1, 1));
+		reserroom.setDateIn(null);
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("3++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("3++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testCommentLengthMax6SP2() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setCommentReserroom("Add Pillow2BlanketTowelBlanketTowelBlanketTowel");
+		reserroom.setDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		reserroom.setDateOut(new Date(2018 - 1900, 2 - 1, 1));
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("4++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("4++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testCommentLengthMin6SP2() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setCommentReserroom("Add dd3f");
+		reserroom.setDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		reserroom.setDateIn(new Date(2018 - 1900, 2 - 1, 1));
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("5++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("5++++++++++++++++");
+		}
+	}
+
+	
+	@Test
+	public void testCommentStartWithAToZSP2() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setCommentReserroom("#dd Pillow2");
+		reserroom.setDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		reserroom.setDateOut(new Date(2018 - 1900, 2 - 1, 1));
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.print("6++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("6++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testCommentMustBeUnique() {
+		ReservationRoomEntity reserroom1 = new ReservationRoomEntity();
+		reserroom1.setCommentReserroom("Add Pillow2");
+		reserroom1.setDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		reserroom1.setDateOut(new Date(2018 - 1900, 2 - 1, 1));
+		entityManager.persist(reserroom1);
+		entityManager.flush();
+		ReservationRoomEntity reserroom2 = new ReservationRoomEntity();
+		reserroom2.setCommentReserroom("Add Pillow2");
+		reserroom2.setDateIn(new Date(2018 - 1900, 2 - 1, 1));
+		reserroom2.setDateOut(new Date(2018 - 1900, 2 - 1, 1));
+
+		try {
+			entityManager.persist(reserroom2);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.persistence.PersistenceException e) {
+			System.out.println("12====================");
+			e.printStackTrace();
+			System.out.println("====================");
+
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			assertEquals(violations.size(), 1);
+			System.out.println("122====================");
+			System.out.println(e);
+			e.printStackTrace();
+			System.out.println("====================");
+
+		}
+	}
+
+
+	@Test
+	public void testVadidValueHotelAll() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setNewHotelEntity(hotelRepository.findByName("PhiMaiIn"));
+		reservationRoomRepository.save(reserroom);
+	}
+
+	@Test
+	public void testHotelCannotBeNull() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setNewHotelEntity(hotelRepository.findByName(null));
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			// assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+
+	@Test
+	public void testVadidValueCustomerAll() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setNewCustomerEntity(customerRepository.findCustomerByName("PhiMaiIn"));
+		reservationRoomRepository.save(reserroom);
+	}
+
+	@Test
+	public void testCustomerCannotBeNull() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setNewCustomerEntity(customerRepository.findCustomerByName(null));
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			// assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+
+
+	@Test
+	public void testVadidValuePromotionAll() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setNewPromotionEntity(promotionRepository.findByPromotionName("PhiMaiIn"));
+		reservationRoomRepository.save(reserroom);
+	}
+
+	@Test
+	public void testPromotionCannotBeNull() {
+		ReservationRoomEntity reserroom = new ReservationRoomEntity();
+		reserroom.setNewPromotionEntity(promotionRepository.findByPromotionName(null));
+
+		try {
+			entityManager.persist(reserroom);
+			entityManager.flush();
+
+			//fail("Should not pass to this line");
+		} catch (javax.validation.ConstraintViolationException e) {
+			Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+			assertEquals(violations.isEmpty(), false);
+			// assertEquals(violations.size(), 1);
+			System.out.print("23++++++++++++++++");
+			System.out.print(e.getConstraintViolations());
+			System.out.print("23++++++++++++++++");
+		}
+	}
+
+	
+}
